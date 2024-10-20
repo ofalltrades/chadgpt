33859c3014cf176340d752a9d5b3f5249cdf8306
Jake Johnson <kognizant.kog@gmail.com>
2024-10-19 22:43:44 -0400

    Init - ChatGPT chat interface

---
+++ README.md
@@ -0,0 +1,3 @@
+# ChadGPT
+
+A ChatGPT GUI allowing for larger memory via ChatGPT API and local storage system. Written in Python with wxWidget for GUI toolkit.
---
+++ chadgpt.py
@@ -0,0 +1,31 @@
+import wx
+
+class ChatApp(wx.Frame):
+    def __init__(self):
+        super().__init__(None, title='Chat App', size=(400, 300))
+        
+        self.panel = wx.Panel(self)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.chat_display = wx.TextCtrl(self.panel, style=wx.TE_MULTILINE | wx.TE_READONLY)
+        self.message_input = wx.TextCtrl(self.panel, style=wx.TE_PROCESS_ENTER)
+        
+        self.sizer.Add(self.chat_display, 1, wx.EXPAND | wx.ALL, 5)
+        self.sizer.Add(self.message_input, 0, wx.EXPAND | wx.ALL, 5)
+        
+        self.message_input.Bind(wx.EVT_TEXT_ENTER, self.on_send)
+
+        self.panel.SetSizer(self.sizer)
+        self.Show()
+
+    def on_send(self, event):
+        message = self.message_input.GetValue()
+        if message:
+            self.chat_display.AppendText(f"You: {message}\n")
+            self.message_input.Clear()
+            # Here you would typically send the message to a server or other clients
+
+if __name__ == '__main__':
+    app = wx.App(False)
+    ChatApp()
+    app.MainLoop()
